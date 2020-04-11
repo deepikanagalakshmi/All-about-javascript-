1e203518b1f4da0180d27b46ee49fd4410b9153e
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-04-11 16:18:42 +0530

    Variables in javascript
    Variable is reserving a place in memory.
    In javascript variables are defined using "let" keyword.

---
+++ variables.js
@@ -0,0 +1,13 @@
+let name="john"
+let score="100"
+let bonus="20"
+console.log(name)
+console.log(score+bonus)
+let score1=100.0
+let bonus1=20
+console.log(score1+bonus1)
+let firstName="john"
+let lastName="Doe"
+let fullName=firstName + " " +lastName
+console.log(fullName)
+console.log(firstName+"**"+lastName)
