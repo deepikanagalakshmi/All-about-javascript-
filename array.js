400fd176e1833f905aa7c31eae03608555b52445
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-04-15 16:37:12 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+const superHeroes=["iron man","super man","Spider man","Caption America"]
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes[2])
+
+console.log("LENGTH of superHeroes: " + superHeroes.length)
+console.log("LAST ELEMENT: "+ superHeroes[superHeroes.length-1]);
+
+//including javascript code inside string
+
+console.log(`we have ${superHeroes.length} super heroes`);
+
+
