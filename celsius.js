c974c910f290d6abdf2ae186057b38bf8b44239c
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-04-15 16:33:38 +0530

    Create celsius.js

---
+++ celsius.js
@@ -0,0 +1,6 @@
+//Fahrenheit to celsius convertion
+ //T(°C) = (T(°F) - 32) × 5/9
+ let Fahrenheit=100
+//calculation
+let celsius= (Fahrenheit-32) * 5/9
+console.log("Fahrenheit to celsius " + celsius)
